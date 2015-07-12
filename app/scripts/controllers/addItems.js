5d458fbfebc66c0f45540b3c83512b6b53379462
vikitripathi <abhishek09011993@gmail.com>
2015-07-12 14:08:26 +0530

    feat(Stock):implemented add/edit stock items and transactions @todo authentication API

--- app/scripts/controllers/addItems.js
+++ app/scripts/controllers/addItems.js
@@ -3,15 +3,36 @@
 angular
    .module('messUiApp')   //no dependency as already defined
     .controller('addItemsCtrl',['$scope','$http','items',function($scope,$http,items) {  
-         //add dependency , eg factory(items) to be added
-         // $scope.things = ['A', 'Set', 'fOf', 'Things'];
-         // $scope.title  = 'State 2 List';
-         // $scope.selectedValue='Set';
-         // $scope.items=items;
-         // console.log(items);
-         // // localStorage.itemDetails = JSON.stringify(response);
-         // $scope.save=function(){
-         //    //alert(JSON.stringify($scope.items));
-                          
-         // };
+         $scope.submit=function(){
+            //console.log(JSON.stringify($scope.form));
+            var data=JSON.parse(JSON.stringify($scope.form));   //convert to object
+            /*
+            JSON.stringify() Converts an object into a string.
+            JSON.parse() Converts a JSON string into an object.
+             */
+            
+            //data.push({"item_id":"4"});
+            console.log(data);            
+            items.getItems().then(function(response){
+                //console.log(response);
+                var len=response.length;                
+                var itemData={};
+                // Object.defineProperties(itemData,{
+                //     "item_id":{
+                //         value:len+1,
+                //         writable:true
+                //     }
+                // });
+                itemData["item_id"]=len+1;
+                for(var key in data){
+                    itemData[key]=data[key];
+                }
+                //data['item_id']=len+1;
+                console.log(itemData);
+                items.addItem(itemData).then(function(response){
+                    console.log(response);
+                });
+            }); 
+         }
+         // $scope.form.item_unit="kg";                                       
     }]);
